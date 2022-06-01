83a9b6522c6a54d7494fd5d9e1193d52afab1f96
ansk1d <105351282+ansk1d@users.noreply.github.com>
2022-06-01 14:08:05 +0300

    so close!

---
+++ script.js
@@ -0,0 +1,49 @@
+//1 Print odds 1-20
+// Using a loop write code that will console.log all of the odd values from 1 up to 20.
+
+// for (var i = 1 ; i <=20 ; i++){
+//     if (i % 2 == 0)
+//     console.log(i)
+// }
+
+// 2 Decreasing Multiples of 3
+// Using a loop write code that will console.log all of the values that are evenly 
+// divisible by 3 from 100 down to 0.
+
+
+// for (var j = 100; j >1 ; j--){
+//     if (j % 3 === 0)
+//     console.log(j)
+// } 
+
+// 3 Print the sequence
+// Using a loop write code that will console.log the values in
+// this sequence 4, 2.5, 1, -0.5, -2, -3.5.
+
+// var bigN =4
+
+// for (let b = -3.5; b < bigN; b++) {
+//     console.log(Math.floor(b) ); 
+// }
+
+// 4 Sigma
+// Write code that will add all of the values from 1-100
+//onto some variable sum and at the end console.log the result 1 + 2 + 3 + ... + 98 + 99 + 100.
+//We should get back 5050 at the end.
+
+// function totel() {
+//     var totel = 0 ;
+//     for ( t = 0 ; t <= 100; t++) {
+//         let totel = t + totel;
+//         return totel                         didn't fisnish IT!!!!!!!!!!!!!
+//     }
+// }
+// console.log(totel())
+
+
+// Factorial
+// Write code that will multiply all of the values from 1-12 onto some variable product and at
+// the end console.log the result 1 * 2 * 3 * ... * 10 * 11 * 12. We should get back 479001600
+// at the end.
+
+//SSAAAAMMMMMEEEEEE  didn't fisnish IT!!!!!!!!!!!!!
